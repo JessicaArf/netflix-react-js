b2be6e4fcf564dcaa8fdbfc5a02728e700c9fe1b
Jessica Arf <jessicalealarf8@gmail.com>
2022-12-15 18:06:36 -0300

    feat: movie banner created

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,38 @@
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import {useEffect, useState} from 'react'
 import {requests} from '../services/api'
 
 export const Main = () => {
 
-   const [movies, setMovies] = useState([])
+    const [movies, setMovies] = useState([])
 
-   const movie = movies[Math.floor(Math.random() * movies.length)];
+    const movie = movies[Math.floor(Math.random() * movies.length)];
 
-   useEffect(() => {
-      axios.get(requests.requestPopular).then((response) => {
-        setMovies(response.data.results)
-      })
-   }, [])
+    useEffect(() => {
+        axios.get(requests.requestPopular).then((response) => {
+            setMovies(response.data.results)
+        })
+    }, [])
 
- console.log(movie)
+    console.log(movie)
 
 
-  return (
-    <div>Main</div>
-  )
+    return (
+        <div className='w-full h-[35rem] text-white'>
+            <div className='w-full h-full'>
+                <div className='absolute w-full h-[35rem] bg-gradient-to-r from-black'></div>
+                <img className='w-full h-full object-cover'
+                    src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title}/>
+            </div>
+            <div className='absolute w-full top-[20%] p-4 md:p-8 font-bold'>
+                <h1 className='text-3xl md:text-5xl'>{movie?.title}</h1>
+                <div className='my-4'>
+                    <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>Play</button>
+                    <button className='border text-white border-gray-300 py-2 px-5 ml-4'>Watch Later</button>
+                </div>
+                <span className='text-gray-400 text-sm'>Lançado em: {movie?.release_date}</span>
+                <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray'>{movie?.overview}</p>
+            </div>
+        </div>
+    )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ import Logo from "../assets/netflix-logo.svg"
 export const NavBar = () => {
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
-     <img src={Logo} alt="" className='max-h-12 m-3'/>
+     <img src={Logo} alt="logo" className='max-h-12 m-3'/>
     <div>
       <button className='text-white pr-4'>Sign In</button>
       <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>Sign Up</button>
